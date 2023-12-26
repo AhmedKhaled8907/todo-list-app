8654ba6689ce5a8fc97f2dca305f3afd54988d69
Ahmed khaled <ahmedkh8907@gmail.com>
2023-12-26 19:36:10 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'package:todo_list_app/add_todo_page.dart';
+import 'package:todo_list_app/todo_list.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ToDo List App',
+      theme: ThemeData(brightness: Brightness.dark),
+      initialRoute: '/',
+      routes: {
+        '/': (_) => const TodoList(),
+        '/add-todo': (_) => const AddTodoPage(),
+      },
+    );
+  }
+}
